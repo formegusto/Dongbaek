816aeb47b88beb788a9a9525a478b15955ec97c8
formegusto <formegusto@gmail.com>
2022-05-13 15:22:31 +0900

    update : cube study okay

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import styled from "styled-components";
 import Cube from "./components/Cube";
 
 function App() {
-  return <Cube />;
+  return (
+    <Wrapper>
+      <Cube />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  position: relative;
+  width: 200px;
+  padding-top: 100px;
+  margin: 0 auto;
+  perspective: 1200px;
+`;
 
 export default App;
