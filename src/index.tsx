102f2a67d48c48e75a93704b9ba7b8147014012d
formegusto <formegusto@gmail.com>
2022-05-13 15:00:37 +0900

    mobx init

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
+import { Provider } from "mobx-react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import GlobalStyles from "./styles/GlobalStyles";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <Provider>
+    <GlobalStyles />
+    <App />
+  </Provider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
