2aaae431294375efe44758a05c6932f16cb1c96d
formegusto <formegusto@gmail.com>
2022-05-18 18:08:06 +0900

    update : image data 사용해야함니당!

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import React from "react";
 import styled from "styled-components";
-import Carousel from "./components/Carousel";
-import Cube from "./components/Cube";
+import CanvasCapture from "./components/CanvasCapture";
+// import Carousel from "./components/Carousel";
+// import Cube from "./components/Cube";
 
 function App() {
   return (
-    <CarouselWrapper>
-      <Carousel />
-    </CarouselWrapper>
+    <CaptureWrapper>
+      <CanvasCapture />
+    </CaptureWrapper>
   );
 }
+
+const CaptureWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 300px;
+  height: 220px;
+
+  & > button {
+    margin: 208px 0 0;
+  }
+`;
 
 const CubeWrapper = styled.div`
   position: relative;
--- src/components/CanvasCapture.tsx
+++ src/components/CanvasCapture.tsx
@@ -1,5 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+
 function CanvasCapture() {
-  return <></>;
+  const captureBox = React.useRef<HTMLDivElement>(null);
+  const refCanvas = React.useRef<HTMLCanvasElement>(null);
+
+  const onCapture = React.useCallback(() => {
+    if (refCanvas && refCanvas.current) {
+      if (captureBox && captureBox.current) {
+        const ctx = refCanvas.current.getContext("2d");
+
+        ctx?.drawImage(captureBox.current as any, 0, 0, 300, 200);
+        const data = refCanvas.current.toDataURL("image/png");
+
+        console.log(data);
+      }
+    }
+  }, []);
+
+  return (
+    <>
+      <Block>
+        <Target ref={captureBox}>캡처본</Target>
+        <Canvas ref={refCanvas} width={300} height={200} />
+      </Block>
+      <button onClick={onCapture}>캡쳐</button>
+    </>
+  );
 }
+
+const Block = styled.div`
+  position: relative;
+
+  & > * {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+`;
+
+const Canvas = styled.canvas``;
+
+const Target = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 300px;
+  height: 200px;
+
+  border: 1px solid #fff;
+  box-sizing: border-box;
+`;
 
 export default CanvasCapture;
