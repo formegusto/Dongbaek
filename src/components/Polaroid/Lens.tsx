92c488430751185c8634142024f1cdfcdac83400
formegusto <formegusto@gmail.com>
2022-06-01 17:03:54 +0900

    update : circular svg profile,, 이게 뭐라고 열심히할까,, ㅎㅋ 즐거웠엄

--- src/components/Polaroid/Lens.tsx
+++ src/components/Polaroid/Lens.tsx
@@ -1,11 +1,32 @@
+import { inject } from "mobx-react";
+import { observer } from "mobx-react-lite";
 import styled from "styled-components";
+import RootStore from "../../store";
 
-function Lens() {
-  return <Block />;
+type Props = {
+  username?: string;
+};
+
+function Lens({ username }: Props) {
+  return (
+    <Block>
+      <svg width="196" height="196" overflow="visible">
+        <path
+          id="lens-circle"
+          fill="transparent"
+          d=" M  1.4888402048036085   80.98247858864082  A  98 98 0 1 1 1.4888105037727115 80.98264703241693"
+        ></path>
+        <text width="500" fontWeight={100} fontSize={14}>
+          <textPath xlinkHref="#lens-circle">{username}</textPath>
+        </text>
+      </svg>
+      <LensShape />
+    </Block>
+  );
 }
 
-const Block = styled.div`
-  position: relative;
+const LensShape = styled.div`
+  position: absolute;
 
   width: 180px;
   height: 180px;
@@ -29,4 +50,18 @@ const Block = styled.div`
   }
 `;
 
-export default Lens;
+const Block = styled.div`
+  position: relative;
+  overflow: visible;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 214px;
+  height: 214px;
+`;
+
+export default inject((store: RootStore) => ({
+  username: store.auth.auth?.username,
+}))(observer(Lens));
